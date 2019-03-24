8c04df906bb1abf8bc6cc84aec29a634765daf74
hudyu17 <48703806+hudyu17@users.noreply.github.com>
2019-03-24 08:40:05 -0400

    Create parsed_trial.js

---
+++ parsed_trial.js
@@ -0,0 +1,32 @@
+[
+  {
+    "Devices": "Lights",
+    "": "",
+    "Max": "Dryer"
+  },
+  {
+    "Devices": "Dryer",
+    "": "",
+    "Max": "Bathroom"
+  },
+  {
+    "Devices": "Laptop",
+    "": "",
+    "Max": 66000
+  },
+  {
+    "Devices": "TV",
+    "": "",
+    "Max": ""
+  },
+  {
+    "Devices": "Toaster",
+    "": "",
+    "Max": ""
+  },
+  {
+    "Devices": "break",
+    "": "",
+    "Max": ""
+  }
+]
